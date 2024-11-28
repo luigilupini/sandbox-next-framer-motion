b80a105a2d963408f469e45594e9d8b34287ed97
Luigi Lupini <luigi.lupini@outlook.com>
2024-11-28 17:02:19 +0100

    add shared store card and list components; update dependencies

---
+++ app/3-shared-store-card/page.tsx
@@ -0,0 +1,9 @@
+import AnimatedCard from "./Card";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col justify-center items-center w-screen h-screen">
+      <AnimatedCard />
+    </main>
+  );
+}
---
+++ app/3-shared-store-list/page.tsx
@@ -0,0 +1,9 @@
+import SharedLayout from "./SharedLayout";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col justify-center items-center bg-white w-screen h-screen">
+      <SharedLayout />
+    </main>
+  );
+}
